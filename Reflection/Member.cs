50f249b8df8802caf745f885774f7a2f12c6530a
Andre <andrebrandao@sapo.pt>
2016-05-14 19:24:08 +0100

    Semi-working cache
    ugly cache

---
+++ Reflection/Member.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ClassLibrary2.Reflection
+{
+    //Ghetto
+    public interface IMember
+    {
+        bool IsStatic
+        {
+            get;
+        }
+    }
+    //http://stackoverflow.com/questions/16073091/is-there-a-way-to-create-a-delegate-to-get-and-set-values-for-a-fieldinfo
+    public class Member<T> : IMember
+    {
+        private MemberInfo memberInfo;
+        public Type ParentType;
+        public Func<object, T> GetValue;
+        public Action<object, T> SetValue;
+        public bool IsStatic
+        {
+            get
+            {
+                return ((FieldInfo)memberInfo).IsStatic;
+            }
+        }
+        public Member(MemberInfo memberInfo0, Type parentType)
+        {
+            this.memberInfo = memberInfo0;
+            ParentType = parentType;
+            switch(memberInfo0.MemberType)
+            {
+                case System.Reflection.MemberTypes.Field:
+                    GetValue = CreateGetter((FieldInfo)memberInfo0);
+                    SetValue = CreateSetter((FieldInfo)memberInfo0);
+                    break;
+                case System.Reflection.MemberTypes.Property:
+                    GetValue = CreateGetter((PropertyInfo)memberInfo0);
+                    SetValue = CreateSetter((PropertyInfo)memberInfo0);
+                    break;
+                default:
+                    throw new Exception("Member Info is not of type FieldInfo or PropertyInfo");
+                    break;
+            }
+        }
+
+        #region CreateGetter
+        private Func<object, T> CreateGetter(FieldInfo field)
+        {
+            string methodName = field.ReflectedType.FullName + ".get_" + field.Name;
+            DynamicMethod getterMethod = new DynamicMethod(methodName, typeof(T), new Type[1] { typeof(object) }, true);
+            ILGenerator gen = getterMethod.GetILGenerator();
+            if (field.IsStatic)
+            {
+                gen.Emit(OpCodes.Ldsfld, field);
+            }
+            else
+            {
+                gen.Emit(OpCodes.Ldarg_0);
+                gen.Emit(OpCodes.Ldfld, field);
+            }
+            gen.Emit(OpCodes.Ret);
+            return (Func<object, T>)getterMethod.CreateDelegate(typeof(Func<object, T>));
+        }
+
+        private Func<object, T> CreateGetter(PropertyInfo property)
+        {
+            return (Func<object, T>)Delegate.CreateDelegate(typeof(Func<object, T>),
+                                             property.GetGetMethod());
+        }
+        #endregion
+
+
+        #region CreateSetter
+        private Action<object, T> CreateSetter(FieldInfo field)
+        {
+            string methodName = field.ReflectedType.FullName + ".set_" + field.Name;
+            DynamicMethod setterMethod = new DynamicMethod(methodName, null, new Type[2] { typeof(object), typeof(T) }, true);
+            ILGenerator gen = setterMethod.GetILGenerator();
+            if (field.IsStatic)
+            {
+                gen.Emit(OpCodes.Ldarg_1);
+                gen.Emit(OpCodes.Stsfld, field);
+            }
+            else
+            {
+                gen.Emit(OpCodes.Ldarg_0);
+                gen.Emit(OpCodes.Ldarg_1);
+                gen.Emit(OpCodes.Stfld, field);
+            }
+            gen.Emit(OpCodes.Ret);
+            return (Action<object, T>)setterMethod.CreateDelegate(typeof(Action<object, T>));
+        }
+
+        private Action<object, T> CreateSetter(PropertyInfo property)
+        {
+            return (Action<object, T>)Delegate.CreateDelegate(typeof(Action<object, T>),
+                                               property.GetSetMethod());
+        } 
+        #endregion
+    }
+}
---
+++ Reflection/ReflectionCache.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2.Reflection
+{
+    static class ReflectionCache
+    {
+        private static Dictionary<string, Type> cachedTypes; 
+        private static Dictionary<string, IMember> cachedMemberInfo;
+
+        public static Type GetTypeCache(this Assembly assembly, string className)
+        {
+            string key = assembly.FullName + "." + className;
+            if (cachedTypes.ContainsKey(key))
+            {
+                return cachedTypes[key];
+            }
+
+            var type = assembly.GetType(className);
+            if(type != null)
+            {
+                throw new Exception("Given class not found");
+            }
+            cachedTypes.Add(key, type);
+            return type;
+        }
+
+        public static void CacheMember<T> (this Type type, string memberName)
+        {
+            string key = type.FullName + "." + memberName;
+            if (cachedMemberInfo.ContainsKey(key))
+            {
+                return;
+            }
+
+            var memberInfo = type.GetMember(memberName).FirstOrDefault();
+            if (memberInfo == null)
+            {
+                throw new Exception("Could not find given member");
+            }
+            var member = new Member<T>(memberInfo, type);
+            cachedMemberInfo.Add(key, member);
+        }
+        public static T GetValue<T>(this Type type, string memberName, object thisObject = null) 
+        {
+            type.CacheMember<T>(memberName);
+            
+            var member = cachedMemberInfo[type.FullName + "." + memberName];
+            return ((Member<T>)member).GetValue(thisObject);
+        }
+
+        public static void SetValue<T>(this Type type, string memberName, T value, object thisObject = null)
+        {
+            type.CacheMember<T>(memberName);
+
+            var member = cachedMemberInfo[type.FullName + "." + memberName];
+            ((Member<T>)member).SetValue(thisObject, value);
+
+        }
+
+    }
+}
